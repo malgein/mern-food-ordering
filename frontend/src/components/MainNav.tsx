4f55bc70078630d7cbbfe396b771c08c8f0c4312
Wilmer Pocaterra <malgein17@gmail.com>
2024-10-31 00:01:07 -0400

    Nav created succesfully

--- frontend/src/components/MainNav.tsx
+++ frontend/src/components/MainNav.tsx
@@ -1,10 +1,38 @@
 import React from 'react'
 import { Button } from './ui/button'
+//Provedor de autenticacion
+import { useAuth0 } from "@auth0/auth0-react";
+import { Link } from "react-router-dom";
+import UsernameMenu from './UsernameMenu';
 
 //*Componente para el login del nav
-export default function MainNav() {
+const MainNav= () => {
+
+	//  isAuthenticated  un elemento de useAuth0 determina si el usuario actual esta autenticaco o no
+	const { loginWithRedirect, isAuthenticated  } = useAuth0();
+
 	return (
-		<Button  variant="ghost"
-		className="font-bold hover:text-orange-500 hover:bg-white">Log In</Button>
+		<span className="flex space-x-2 items-center">
+			{/* Si el usuario se encuentra autenticado */}
+			{isAuthenticated ? (
+				<>
+				{/* Entonces mostramos un boton con el estatus de su orden */}
+					 <Link to="/order-status" className="font-bold hover:text-orange-500">
+            Order Status
+          </Link>
+					 {/* Y tambien el menu de usuario autenticado */}
+					<UsernameMenu />
+				</>
+			) : (
+				// Si no esta autenticado entonces mostramos el boton para login que lo llevara a autenticar con google
+				<Button  variant="ghost"
+				className="font-bold hover:text-orange-500 hover:bg-white"
+				onClick={async () => await loginWithRedirect()}
+					>Log In
+				</Button>
+			)}
+		</span>
 	)
 }
+
+export default MainNav 
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
+import Auth0ProviderWithNavigate from "./auth/AuthProviderWithNavigate"
 import ReactDOM from 'react-dom/client'
 import './global.css'
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRoutes from './AppRoutes';
+// Archivo de autenticacion de auth0
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Router>
-      <AppRoutes />
+      <Auth0ProviderWithNavigate>
+        <AppRoutes />
+      </Auth0ProviderWithNavigate>
     </Router>
   </React.StrictMode>,
 )
